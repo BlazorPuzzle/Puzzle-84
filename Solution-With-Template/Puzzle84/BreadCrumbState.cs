f1d15bd4dcea2d635fa55c6b51cb988594cde0b8
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2025-07-09 14:23:57 -0400

    Added Solution with Template

---
+++ Solution-With-Template/Puzzle84/BreadCrumbState.cs
@@ -0,0 +1,18 @@
+﻿namespace Puzzle84;
+
+/// <summary>
+/// Represents the state of breadcrumbs, maintaining a stack of <see cref="BreadCrumb"/> instances
+/// to track navigation history or path within an application.
+/// </summary>
+public class BreadCrumbState
+{
+	public Stack<BreadCrumb> BreadCrumbs { get; } = new();
+}
+
+
+/// <summary>
+/// Represents a breadcrumb with a title and URL.
+/// </summary>
+/// <param name="Title"></param>
+/// <param name="Url"></param>
+public record BreadCrumb(string Title, string Url);
